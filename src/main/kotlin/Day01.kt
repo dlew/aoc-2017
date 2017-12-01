2938dc275c85a11cd8eb8838b53601a3ecd5bce2
Dan Lew <danlew42@gmail.com>
2017-11-30 23:10:35 -0600

    Added solution for day 1 problem

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,3 +1,23 @@
 object Day01 {
 
+  fun sum(input: String): Int {
+    val intInput = inputToIntList(input)
+    val circledInput = intInput + listOf(intInput[0])
+    return circledInput.windowed(2)
+        .filter { it[0] == it[1] }
+        .sumBy { it[0] }
+  }
+
+  fun sum2(input: String): Int {
+    val intInput = inputToIntList(input)
+    val size = intInput.size
+    val halfSize = size / 2
+    val doubleInput = intInput + intInput
+    return (0 until size)
+        .filter { doubleInput[it] == doubleInput[it + halfSize] }
+        .sumBy { doubleInput[it] }
+  }
+
+  private fun inputToIntList(input: String) = input.map { Integer.valueOf(it.toString()) }
+
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,11 +1,33 @@
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class Day01Test {
 
   @Test
-  fun sample() {
-    assertNotNull("Hello!")
+  fun part1samples() {
+    assertEquals(3, Day01.sum("1122"))
+    assertEquals(4, Day01.sum("1111"))
+    assertEquals(0, Day01.sum("1234"))
+    assertEquals(9, Day01.sum("91212129"))
   }
 
+  @Test
+  fun part1() {
+    assertEquals(1136, Day01.sum(getResourceAsString("day01.txt")))
+  }
+
+  @Test
+  fun part2samples() {
+    assertEquals(6, Day01.sum2("1212"))
+    assertEquals(0, Day01.sum2("1221"))
+    assertEquals(4, Day01.sum2("123425"))
+    assertEquals(12, Day01.sum2("123123"))
+    assertEquals(4, Day01.sum2("12131415"))
+  }
+
+  @Test
+  fun part2() {
+    assertEquals(1092, Day01.sum2(getResourceAsString("day01.txt")))
+  }
 }
