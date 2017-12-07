8e5505a9ea3385d5fbb80c71ed776737e0ef5d1f
Dan Lew <danlew42@gmail.com>
2017-12-06 21:03:24 -0600

    Moved Template functions into TemplateTest itself
    (So I don't need to switch between files while working on things.)

--- src/test/kotlin/TemplateTest.kt
+++ src/test/kotlin/TemplateTest.kt
@@ -4,24 +4,32 @@ import utils.getResourceAsString
 
 class TemplateTest {
 
+  fun part1(input: String): String {
+    return input
+  }
+
+  fun part2(input: String): String {
+    return input
+  }
+
   @Test
   fun part1sample() {
-    assertEquals("Hello!", Template.part1(getResourceAsString("template-sample1.txt")))
+    assertEquals("Hello!", part1(getResourceAsString("template-sample1.txt")))
   }
 
   @Test
   fun part1() {
-    assertEquals("Hello, world!", Template.part1(getResourceAsString("template.txt")))
+    assertEquals("Hello, world!", part1(getResourceAsString("template.txt")))
   }
 
   @Test
   fun part2sample() {
-    assertEquals("Goodbye!", Template.part2(getResourceAsString("template-sample2.txt")))
+    assertEquals("Goodbye!", part2(getResourceAsString("template-sample2.txt")))
   }
 
   @Test
   fun part2() {
-    assertEquals("Hello, world!", Template.part2(getResourceAsString("template.txt")))
+    assertEquals("Hello, world!", part2(getResourceAsString("template.txt")))
   }
 
 }
