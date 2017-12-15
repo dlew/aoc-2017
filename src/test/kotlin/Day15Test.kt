4d2b9ddb4855448126d8ab1706ef31cab7a1bdae
Dan Lew <danlew42@gmail.com>
2017-12-14 23:29:54 -0600

    Added solution for day 15

---
+++ src/test/kotlin/Day15Test.kt
@@ -0,0 +1,27 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.coroutines.experimental.buildSequence
+
+class Day15Test {
+
+  @Test
+  fun part1sample() {
+    assertEquals(588, Day15.part1(65, 8921))
+  }
+
+  @Test
+  fun part1() {
+    assertEquals(567, Day15.part1(512, 191))
+  }
+
+  @Test
+  fun part2sample() {
+    assertEquals(309, Day15.part2(65, 8921))
+  }
+
+  @Test
+  fun part2() {
+    assertEquals(323, Day15.part2(512, 191))
+  }
+
+}
